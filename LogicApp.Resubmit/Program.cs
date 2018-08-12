3303c90562fd73d92a6a9252042c2bd45f52fbd3
Balakrishnan, Rajkumar <r.balakrishnan@agrifirm.com>
2018-08-12 10:48:19 +0200

    Add project files.

---
+++ LogicApp.Resubmit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicApp.Resubmit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
